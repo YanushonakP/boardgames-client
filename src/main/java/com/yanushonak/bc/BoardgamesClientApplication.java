d493bd798bda890a6a80123f9c62e00d34e4a9a8
Pavel Yanushonak <1237023m@gmail.com>
2020-04-21 16:13:46 +0300

    main, entity, controller, user repository added

---
+++ src/main/java/com/yanushonak/bc/BoardgamesClientApplication.java
@@ -0,0 +1,13 @@
+package com.yanushonak.bc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class BoardgamesClientApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(BoardgamesClientApplication.class, args);
+        }
+
+    }
+}
