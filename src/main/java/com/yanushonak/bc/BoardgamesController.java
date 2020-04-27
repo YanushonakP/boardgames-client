fa39c21336d6b0b31976b83fceadb2e0ebc9156f
Pavel Yanushonak <1237023m@gmail.com>
2020-04-27 08:52:14 +0300

    try to implement thymeleaf (unsuccessful)

--- src/main/java/com/yanushonak/bc/BoardgamesController.java
+++ src/main/java/com/yanushonak/bc/BoardgamesController.java
@@ -2,6 +2,7 @@ package com.yanushonak.bc;
 
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.GetMapping;
         import org.springframework.web.bind.annotation.PostMapping;
         import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,4 +34,11 @@ public class BoardgamesController {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(
+            name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
 }
